7bbefbb6d43e318ad43b22d77b766324fa41888a
AndresC8 <andresr4dp@gmail.com>
2023-12-06 11:19:15 -0500

    routes

--- models/user.py
+++ models/user.py
@@ -6,9 +6,7 @@ users = Table("Users", meta,
 Column("id", Integer, primary_key=True), 
 Column("name", String(225)),
 Column("email", String(225)),
-Column("age", Integer()),
 Column("password", String(225)),
-mysql_charset = "utf8mb4"
 )
 
 meta.create_all(engine)
