5ee73db70c4f7ccd341c3f91fd91c8980909bf8c
AndresC8 <andresr4dp@gmail.com>
2023-12-12 12:29:58 -0500

    config

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title='users API',
+    description='restAPI with python'
+    version='0.0.1',
+)
 
 app.include_router(user)
 
